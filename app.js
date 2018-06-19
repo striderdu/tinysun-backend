40e4ba8ed812a9dc04d56dd69aca6093474b8b69
striderdu <duhaotong@hotmail.com>
2018-06-19 18:21:16 +0800

    update app.js
    添加了注释

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
+/**
+ * @fileOverview Tinysun后端
+ * @name app.js
+ * @author 杜昊桐
+ */
+
+//引入模块
 var express = require('express')
 var bodyParser = require('body-parser');
 var Sequelize = require('sequelize');
 var app = express()
 
+//设置数据库连接属性
 const sequelize = new Sequelize('tinysun', 'tinysun', '123', {
     host: '39.105.8.165',
     dialect: 'mysql',
     operatorsAliases: false
 });
 
+//设置body-parser模块
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+
+//index
 app.get('/', function(req, res){
     res.send('Welcome to tinysun!');
 });
 
+/**
+ * 用户登录检查
+ * @param {object} req 请求
+ * @returns {json{status: 0, 1 or 2}} json数据
+ * @description 根据用户角色返回不同状态，老师返回2、学生返回1，登录错误返回0
+ */
 app.post('/login-check', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
@@ -37,6 +54,12 @@ app.post('/login-check', function(req, res){
     });
 });
 
+/**
+ * 老师获取学生信息
+ * @param {object} req 请求
+ * @returns {json{stus:{}, prob:{}}} json数据
+ * @description stus返回学生信息，prob返回题目总数
+ */
 app.get('/teacher',function(req, res){
     var teacher_id = req.query.id;
     var data = {
@@ -49,9 +72,6 @@ app.get('/teacher',function(req, res){
         type: sequelize.QueryTypes.SELECT
     }).then(students => {
         stus = students;
-        //console.log(stus);
-        //res.json({info: students});
-        //console.log(students[0].already);
     });
     sequelize.query('select problem, total from problems where teacher_id = :id', {
         replacements: data,
@@ -59,12 +79,15 @@ app.get('/teacher',function(req, res){
     }).then(problems => {
         prob = problems;
         res.json({stu: stus, prob: prob});
-        //console.log(prob);
-        //console.log(problems);
     })
-    //res.json({info: stus, prob: prob});
 })
 
+/**
+ * 学生获取题目
+ * @param {object} req 请求
+ * @returns {json{problems:{}}} json数据
+ * @description problems返回题目数量与内容
+ */
 app.get('/student',function(req, res){
     var student_id = req.query.id;
     var data = {
@@ -78,6 +101,12 @@ app.get('/student',function(req, res){
     });
 })
 
+/**
+ * 上传完成率
+ * @param {object} req 请求
+ * @returns {json{status: 0 or 1 }} json数据
+ * @description 成功返回1，失败返回0
+ */
 app.post('/upload-grade', function(req, res){
     var student_id = req.body.id;
     var already = req.body.already;
@@ -85,19 +114,6 @@ app.post('/upload-grade', function(req, res){
         id: student_id,
         already: already 
     };
-    /*
-    sequelize.query('select already from students where student_id :id', {
-        replacements: data1,
-        type: sequelize.QueryTypes.SELECT
-    }).then(students => {
-        already_oall = students[0].already;
-    });
-    var already_all = already_oall + already;
-    var data2 = {
-        id: student_id,
-        already: already_all,
-
-    }*/
     sequelize.query('updata students set already = already + :alread where student_id = :id', {
         replacements: data,
         type: sequelize.QueryTypes.UPDATE
@@ -109,6 +125,12 @@ app.post('/upload-grade', function(req, res){
     });
 })
 
+/**
+ * 上传题目
+ * @param {object} req 当前的请求
+ * @returns {json{status: 0 or 1 }} json数据
+ * @description 成功返回1，失败返回0
+ */
 app.post('/upload-problems', function(req, res){
     var teacher_id = req.body.id;
     var problem = req.body.problem;
@@ -129,6 +151,7 @@ app.post('/upload-problems', function(req, res){
     });
 })
 
+//监听3000端口
 app.listen(3000, function () {
     console.log('app is listening at port 3000');
 });
